0c003e84192e307012d99717d97920c8eca3ac59
rehanarroihan <third2014project@gmail.com>
2019-06-20 01:31:12 +0700

    updating place api param

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const request = require('request');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  request(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.query.q}&key=${process.env.MAPS_KEY_API}`, (err, rese, body) => {
+  request(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${req.query.q}&inputtype=textquery&fields=formatted_address,name,geometry&key=${process.env.MAPS_KEY_API}`, (err, rese, body) => {
     return res.json(JSON.parse(rese.body));
   });
 });
